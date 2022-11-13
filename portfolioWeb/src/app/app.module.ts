3d13f7c07881e3eae2acfab57b9fb29fc0ad21db
Enrique Ramallo <quikebr5@gmail.com>
2022-11-13 20:39:29 -0300

    creaciondeHeader

--- portfolioWeb/src/app/app.module.ts
+++ portfolioWeb/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import { SobreMiComponent } from './component/sobre-mi/sobre-mi.component';
+import { ContactameComponent } from './component/contactame/contactame.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProyectosComponent,
+    SobreMiComponent,
+    ContactameComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
